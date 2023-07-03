3bc4d9fbe7a043eff07e43ba68acbefd043ea6f8
Raul_Cordeiro <raulcordeirocontato@gmail.com>
2023-07-03 11:58:24 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,66 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import Compartilhar from './compartilhar.jsx';
-import NavBar from './NavBar';
-
-import ShareButton from './compartilhar.jsx';
-
-////https://privacypros.io/tor/best-onion-sites///
-
-
-
-
-function NoticiaCard({ noticia, onShare }) {
-  
-  return (
-    
-    <div className="noticia-card">
-      <section>
-<NavBar /> {/* Incluindo o componente NavBar */}
-</section>
-
-         
-      <div>
-        
-        <p className='bem-vindo' >
-        Raul Cordeiro Tecnologias</p>
-      </div>
-      <div>
-        <p className='bem-vindo2'> Notícias da BBC de Londres</p>
-        
-      </div>
-        <img className="noticia-image" src={noticia.urlToImage} alt={noticia.title} />
-        <h1 className="noticia-title">{noticia.title}</h1>
-       
-        <h2 className="noticia-description">{noticia.description}</h2>
-        <a className="noticia-url" href={noticia.url} target="_blank" rel="noopener noreferrer">
-        {noticia.url}
-      </a>
-      <p className='quality'>Saiba Mais</p>
-      <p className='quality'>Oferecemos conteúdo de qualidade sem alienação</p>
-      <ShareButton url={noticia.url} />
-      
-
-
-    </div>
-  );
-}
+import Compartilhar from "./Compartilhar.jsx"
 
 function Noticia() {
-  const [noticias, setNoticias] = useState([]);
-  const [showCompartilhar, setShowCompartilhar] = useState(false);
-  const [sharedUrl, setSharedUrl] = useState('');
-  
   useEffect(() => {
     const fetchNews = async () => {
       try {
         const apikey = "ffda70369f1f4904b3d227ab8b9e2659";
-        //const pais = "pt";//
+        const pais = "pt";
         const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apikey}`;
         const response = await fetch(url);
         const data = await response.json();
         const noticias = data.articles;
-        setNoticias(noticias);
+
+        noticias.forEach(noticia => {
+          
+          const div = document.createElement('div');
+          div.className = 'noticia-card';
+
+          const img = document.createElement('img');
+          img.className = 'noticia-image';
+          img.src = noticia.urlToImage;
+
+          const h1 = document.createElement('h1');
+          h1.className = 'noticia-title';
+          h1.textContent = noticia.title;
+
+          const h2 = document.createElement('h2');
+          h2.className = 'noticia-description';
+          h2.textContent = noticia.description;
+
+          const a = document.createElement('a');
+          a.className = 'noticia-url';
+          a.href = noticia.url;
+          a.textContent = noticia.url;
+          <Compartilhar/>
+
+          div.appendChild(img);
+          div.appendChild(h1);
+          div.appendChild(h2);
+          div.appendChild(a);
+
+          if (noticia.category === 'esporte') {
+            document.getElementById("esportes").appendChild(div);
+          } else if (noticia.category === 'política') {
+            document.getElementById("politica").appendChild(div);
+          } else {
+            document.getElementById("outros").appendChild(div);
+          }
+        });
       } catch (error) {
         console.error(error);
       }
@@ -69,18 +57,15 @@ function Noticia() {
     fetchNews();
   }, []);
 
-  const handleShare = (url) => {
-    setSharedUrl(url);
-    setShowCompartilhar(true);
-  };
-
   return (
     <div id='noticias'>
-      {noticias.map(noticia => (
-        <NoticiaCard key={noticia.title} noticia={noticia} onShare={handleShare} />
-      ))}
-      {showCompartilhar && <Compartilhar url={sharedUrl} />}
+      
+      <div id="tecnologia"></div>
+      <div id="esportes"></div>
+      <div id="politica"></div>
+      <div id="outros"></div>
     </div>
+    
   );
 }
 
