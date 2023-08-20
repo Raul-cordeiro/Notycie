138f471b0e55cd8708d44af2810588c82cb89c07
Raul_Cordeiro <raulcordeirocontato@gmail.com>
2023-08-20 11:02:29 -0300

    okk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
-import Tmdb from './tmdb'; // Importe o módulo Tmdb corretamente
+import Tmdb from './tmdb.jsx'; // Substitua 'seuarquivo.js' pelo caminho correto do seu arquivo
 
 export default () => {
   useEffect(() => {
     const loadAll = async () => {
       try {
-        let list = await Tmdb.getHomeList(); // Use a função getHomeList do módulo Tmdb
+        let list = await Tmdb.getHomeList(); // Use a função getHomeList do objeto Tmdb
         console.log(list);
       } catch (error) {
         console.error("Erro ao carregar lista:", error);
